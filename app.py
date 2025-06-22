7817b0276effd6a40588578721ef1233fc6b8fc8
hui-chanSeo <hcseo0411@gmail.com>
2025-06-22 17:41:03 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -27,3 +27,17 @@ with col1:
 
 with col2:
     st.image("book.jpg", caption="📚 책 읽기", use_container_width=True)
+
+st.subheader("🌱 앞으로의 꿈")
+st.write("""
+저는 앞으로도 건양대학교 재난안전소방학과에 계속 다니며 박사과정까지 밟는 것을 목표로 하고 있습니다.  
+특히 디지털 전환과 신재생에너지 자립을 실현하여 전기 및 소방 분야에서 획기적인 에너지 절약 기술을 개발하고,  
+국제적으로 이슈가 되는 탄소세의 제약을 뛰어넘어 세계 환경산업을 선도하는 인재가 되고자 합니다.
+
+이를 통해 우리나라가 다시 경제적 활력을 되찾는 데 기여하고,  
+사회의 지속가능한 발전에 실질적인 도움을 주는 전문가로 성장하는 것이 저의 꿈입니다.
+""")
+
+st.subheader("🎥 영상 소개")
+st.video("https://www.youtube.com/watch?v=46mebsp1Q4M")
+st.video("https://www.youtube.com/watch?v=4N2ygGH6b70")
